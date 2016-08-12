aafc9d5b59da022e2db6bf7d1ec8fce11471fd8f
Ameya <ameya.kulkarni@weboniselab.com>
2016-08-12 15:05:32 +0530

    Changes for adding a /todo api

--- server.go
+++ server.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
-	"html"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("serving %s",r.URL.Path)
-  	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	//router.HandleFunc("/todos/{todoID}",TodoShow)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Showing list of all Todos")
+}
+
+/*
+func TodoShow(w http.Req)
+
+*/
